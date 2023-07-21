74924374141ef0b2de9d8616aa2ea2ee08027f2b
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-07-21 14:55:36 -0300

    feat(chess, application): printing the possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,6 +26,12 @@ public class ChessMatch {
     return mat;
   }
 
+  public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+    Position position = sourcePosition.toPosition();
+    validateSourcePosition(position);
+    return board.piece(position).possibleMoves();
+  }
+
   private void validateSourcePosition(Position position) {
     if (!board.thereIsAPiece(position)) {
       throw new ChessException("There is no piece on source position");
